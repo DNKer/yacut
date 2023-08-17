caa451440814ac44ff400e9fb2e473240c0edc7e
Dmitry Korepanov <dnker@mail.ru>
2023-08-17 18:58:08 +0500

    naming PEP

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,11 +1,11 @@
-from http import HTTPStatus as status
+from http import HTTPStatus as Status
 from re import match
 from typing import Any
 
 from flask import jsonify, request
 
 from . import app, db
-from .error_handlers import InvalidAPIUsage
+from .error_handlers import InvalidAPIUsageError
 from .models import URLMap
 from .utils import get_unique_short_id
 
@@ -24,8 +24,8 @@ def api_get_url(short_id) -> Any:
     """ Получить объект по id и конвертировать данные в JSON. """
     url_redirect = URLMap.query.filter_by(short=short_id).first()
     if url_redirect is None:
-        raise InvalidAPIUsage(message=NOT_FOUND_SHORT_ID, status_code=status.NOT_FOUND)
-    return jsonify({'url': url_redirect.original}), status.OK
+        raise InvalidAPIUsageError(message=NOT_FOUND_SHORT_ID, status_code=Status.NOT_FOUND)
+    return jsonify({'url': url_redirect.original}), Status.OK
 
 
 @app.route('/api/id/', methods=['POST'])
@@ -33,17 +33,17 @@ def api_create_short_id() -> Any:
     """ Создать объект и конвертировать данные в JSON. """
     data = request.get_json()
     if not data:
-        raise InvalidAPIUsage(message=MISSING_REQUEST_BODY, status_code=status.BAD_REQUEST)
+        raise InvalidAPIUsageError(message=MISSING_REQUEST_BODY, status_code=Status.BAD_REQUEST)
     if 'url' not in data:
-        raise InvalidAPIUsage(message=URL_REQUIRED_FIELD, status_code=status.BAD_REQUEST)
+        raise InvalidAPIUsageError(message=URL_REQUIRED_FIELD, status_code=Status.BAD_REQUEST)
     if not data.get('custom_id'):
         data['custom_id'] = get_unique_short_id()
-    elif URLMap.query.filter_by(short=data['custom_id']).first():
-        raise InvalidAPIUsage(ID_NOT_FREE.format(data['custom_id']))
+    if URLMap.query.filter_by(short=data['custom_id']).first():
+        raise InvalidAPIUsageError(ID_NOT_FREE.format(data['custom_id']))
     elif not match(PATTERN_SHORT_URL, data['custom_id']):
-        raise InvalidAPIUsage(ERROR_SHORT_URL)
+        raise InvalidAPIUsageError(ERROR_SHORT_URL)
     url_map = URLMap()
     url_map.from_dict(data)
     db.session.add(url_map)
     db.session.commit()
-    return jsonify(url_map.to_dict()), status.CREATED
+    return jsonify(url_map.to_dict()), Status.CREATED
--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -1,4 +1,4 @@
-from http import HTTPStatus as status
+from http import HTTPStatus as Status
 from typing import Any, Dict
 
 from flask import jsonify, render_template
@@ -6,13 +6,13 @@ from flask import jsonify, render_template
 from . import app, db
 
 
-class InvalidAPIUsage(Exception):
+class InvalidAPIUsageError(Exception):
     """
     Класс исключений. Конструктор класса принимает на вход текст
     сообщения и статус-код ошибки (необязательно). Если статус-код
     для ответа API не указан — вернется код 400.
     """
-    status_code = status.BAD_REQUEST
+    status_code = Status.BAD_REQUEST
 
     def __init__(self, message, status_code=None) -> None:
         super().__init__()
@@ -25,7 +25,7 @@ class InvalidAPIUsage(Exception):
         return dict(message=self.message)
 
 
-@app.errorhandler(InvalidAPIUsage)
+@app.errorhandler(InvalidAPIUsageError)
 def invalid_api_usage(error) -> Any:
     """ Обработчик кастомного исключения для API. """
     return jsonify(error.to_dict()), error.status_code
@@ -34,11 +34,11 @@ def invalid_api_usage(error) -> Any:
 @app.errorhandler(404)
 def page_not_found(error) -> str:
     """ Обработка ошибки "Не найдено". """
-    return render_template('404.html'), status.NOT_FOUND
+    return render_template('404.html'), Status.NOT_FOUND
 
 
 @app.errorhandler(500)
 def internal_error(error) -> str:
     """ Обработка ошибки "Ошибка сервера". """
     db.session.rollback()
-    return render_template('500.html'), status.INTERNAL_SERVER_ERROR
+    return render_template('500.html'), Status.INTERNAL_SERVER_ERROR
